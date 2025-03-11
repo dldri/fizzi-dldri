8db23bb6c23e54d804a18e1bc63b5d579dfff2b0
Dldri <diovian.liu@gmail.com>
2025-03-12 05:58:49 +0800

    adding global font Alpino - in progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,28 @@
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import localFont from "next/font/local"
+
+import { PrismicPreview } from "@prismicio/next"
+import { repositoryName } from "@/prismicio"
+import "./globals.css"
+
+const alpino = localFont({
+	src: "../../public/fonts/Alpino-Variable.woff2",
+	display: "swap",
+	weight: "100 900",
+	variable: "--font-alpino",
+})
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={alpino.variable}
+		>
+			<body>{children}</body>
+			<PrismicPreview repositoryName={repositoryName} />
+		</html>
+	)
 }
