065f012551987a1f3bd775d8fe841f432fb0e2b8
Dio Luwandri <diovian.liu@gmail.com>
2025-03-13 18:51:37 +0800

    added Fizzi Logo svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,6 @@
-import localFont from "next/font/local"
-
 import { PrismicPreview } from "@prismicio/next"
 import { repositoryName } from "@/prismicio"
 import "./globals.css"
-
-const alpino = localFont({
-	src: "../../public/fonts/Alpino-Variable.woff2",
-	display: "swap",
-	weight: "100 900",
-	variable: "--font-alpino",
-})
 
 export default function RootLayout({
 	children,
