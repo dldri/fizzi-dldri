d7d543a722659b526785c88203bd661a728cec62
Dldri <diovian.liu@gmail.com>
2025-03-14 05:14:46 +0800

    hero styling complete 1:30:00

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,6 +5,7 @@ import { PrismicNextImage } from "@prismicio/next";
 
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
+import { TextSplitter } from "./TextSplitter";
 
 /**
  * Props for `Hero`.
@@ -19,12 +20,17 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="hero"
     >
       <div className="grid">
         <div className="grid h-screen place-items-center">
-          <div className="grid auto-rows-min place-items-center">
+          <div className="grid auto-rows-min place-items-center text-center">
             <h1 className="hero-header text-7xl leading-[.8] font-black text-orange-500 uppercase md:text-[9rem] lg:text-[13rem]">
-              {asText(slice.primary.heading)}
+              <TextSplitter
+                text={asText(slice.primary.heading)}
+                wordDisplayStyle="block"
+                className="hero-header-word"
+              />
             </h1>
             <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
               <PrismicRichText field={slice.primary.subheading} />
@@ -41,9 +47,18 @@ const Hero: FC<HeroProps> = ({ slice }) => {
         </div>
 
         <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
-          <PrismicNextImage field={slice.primary.cans_image} />
-          <PrismicRichText field={slice.primary.second_heading} />
-          <PrismicRichText field={slice.primary.second_body} />
+          <PrismicNextImage
+            className="w-full md:hidden"
+            field={slice.primary.cans_image}
+          />
+          <div>
+            <h2 className="text-side-heading text-6xl font-black text-balance text-sky-950 uppercase lg:text-8xl">
+              <TextSplitter text={asText(slice.primary.second_heading)} />
+            </h2>
+            <div className="text-side-body mt-4 max-w-xl text-xl font-normal text-balance text-sky-950">
+              <PrismicRichText field={slice.primary.second_body} />
+            </div>
+          </div>
         </div>
       </div>
     </Bounded>
