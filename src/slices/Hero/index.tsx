10c594ab9f7c687e988a234a437b328ada244ac5
Dldri <diovian.liu@gmail.com>
2025-03-15 05:41:51 +0800

    hero second half animated 1:59:00

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -62,17 +62,31 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       },
     });
 
-    scrollTl.fromTo(
-      "body",
-      {
-        backgroundColor: "#FDE047",
-      },
-      {
-        backgroundColor: "#D9f99D",
-        overwrite: "auto",
-      },
-      1,
-    );
+    scrollTl
+      .fromTo(
+        "body",
+        {
+          backgroundColor: "#FDE047",
+        },
+        {
+          backgroundColor: "#D9f99D",
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 20,
+        opacity: 0,
+      });
   });
 
   return (
