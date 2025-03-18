1173242f0a5f80292c57bdad638ced97f4fd248a
Dldri <diovian.liu@gmail.com>
2025-03-19 05:58:49 +0800

    SodaCan render 2:24:34

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { SodaCan } from "./SodaCan";
 
 type Props = object;
 
@@ -23,10 +24,7 @@ export default function ViewCanvas({}: Props) {
         fov: 30,
       }}
     >
-      <mesh rotation={[0.5, 0.5, 0]} position={[1, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"hotpink"} />
-      </mesh>
+      <SodaCan />
       <ambientLight intensity={2} />
       <spotLight intensity={3} position={[1, 1, 1]} />
     </Canvas>
