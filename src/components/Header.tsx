e6d549cb2f08bf81ebe5a69751f23b66dc12c7db
Dldri <diovian.liu@gmail.com>
2025-03-20 05:42:45 +0800

    Cans group and animated 3:15:00

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
-import React from "react"
-import { FizziLogo } from "./FizziLogo"
+import React from "react";
+import { FizziLogo } from "./FizziLogo";
 
-// type Props = {}
-
-export default function Header(/*{}: Props*/) {
-	return (
-		<header className="flex justify-center py-4 -mb-28">
-			<FizziLogo className="h-20 z-10 cursor-pointer text-sky-800" />
-		</header>
-	)
+export default function Header() {
+  return (
+    <header className="-mb-28 flex justify-center py-4">
+      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
+    </header>
+  );
 }
