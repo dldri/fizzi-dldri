5bf29779a4675fec863b2ec662e27c052ac1e968
Dldri <diovian.liu@gmail.com>
2025-07-19 12:57:51 +0800

    animated clouds 4:25:29

--- src/slices/SkyDive/Scene.tsx
+++ src/slices/SkyDive/Scene.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import FloatingCan from "@/components/FloatingCan";
 import { Content } from "@prismicio/client";
 import {
   Cloud,
@@ -11,13 +10,21 @@ import {
 } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import FloatingCan from "@/components/FloatingCan";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 type SkyDiveProps = {
   sentence: string | null;
   flavor: Content.SkyDiveSliceDefaultPrimary["flavor"];
 };
 
-export default function Scene({ sentence, flavour }: SkyDiveProps) {
+export default function Scene({ sentence, flavor }: SkyDiveProps) {
   const groupRef = useRef<THREE.Group>(null);
   const canRef = useRef<THREE.Group>(null);
   const cloud1Ref = useRef<THREE.Group>(null);
@@ -25,11 +32,80 @@ export default function Scene({ sentence, flavour }: SkyDiveProps) {
   const cloudsRef = useRef<THREE.Group>(null);
   const wordsRef = useRef<THREE.Group>(null);
 
+  const ANGLE = 75 * (Math.PI / 180);
+
+  const getXPosition = (distance: number) => distance * Math.cos(ANGLE);
+  const getYPosition = (distance: number) => distance * Math.sin(ANGLE);
+  const getXYPositions = (distance: number) => ({
+    x: getXPosition(distance),
+    y: getYPosition(-1 * distance),
+  });
+
+  useGSAP(() => {
+    if (
+      !cloudsRef.current ||
+      !canRef.current ||
+      !wordsRef.current ||
+      !cloud1Ref.current ||
+      !cloud2Ref.current
+    )
+      return;
+
+    // Set initial positions
+    gsap.set(cloudsRef.current.position, { z: 10 });
+    gsap.set(canRef.current.position, {
+      ...getXYPositions(-4),
+    });
+    gsap.set(
+      wordsRef.current.children.map((word) => word.position),
+      { ...getXYPositions(7), z: 2 },
+    );
+
+    // Spinning can
+    gsap.to(canRef.current.rotation, {
+      y: Math.PI * 2,
+      duration: 1.7,
+      repeat: -1,
+      ease: "none",
+    });
+
+    // Infinite cloud movement
+    const DISTANCE = 15;
+    const DURATION = 6;
+
+    gsap.set([cloud1Ref.current.position, cloud2Ref.current.position], {
+      ...getXYPositions(DISTANCE),
+    });
+
+    gsap.to(cloud1Ref.current.position, {
+      x: `+=${getXPosition(DISTANCE * -2)}`,
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      ease: "none",
+      repeat: -1,
+      duration: DURATION,
+    });
+
+    gsap.to(cloud2Ref.current.position, {
+      x: `+=${getXPosition(DISTANCE * -2)}`,
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      ease: "none",
+      repeat: -1,
+      delay: DURATION / 2,
+      duration: DURATION,
+    });
+  });
+
   return (
     <group ref={groupRef}>
       {/* Can */}
       <group rotation={[0, 0, 0.5]}>
-        <FloatingCan ref={canRef} flavor={flavour}></FloatingCan>
+        <FloatingCan
+          ref={canRef}
+          flavor={flavor}
+          rotationIntensity={0}
+          floatIntensity={3}
+          floatSpeed={3}
+        ></FloatingCan>
       </group>
 
       {/* Clouds */}
@@ -41,7 +117,7 @@ export default function Scene({ sentence, flavour }: SkyDiveProps) {
 
       {/* Text */}
       <group ref={wordsRef}>
-        {sentence && <ThreeText sentence={sentence} />}
+        {sentence && <ThreeText sentence={sentence} color="#F97315" />}
       </group>
 
       {/* Lights */}
@@ -58,5 +134,23 @@ function ThreeText({
   sentence: string;
   color?: string;
 }) {
-  return <Text>{sentence}</Text>;
+  const words = sentence.toUpperCase().split(" ");
+  const material = new THREE.MeshLambertMaterial();
+  const isDesktop = useMediaQuery("(min-width: 950px)", true);
+
+  return words.map((word: string, wordIndex: number) => (
+    <Text
+      key={`${wordIndex}-${word}`}
+      scale={isDesktop ? 1 : 0.5}
+      color={color}
+      material={material}
+      font="/fonts/Alpino-Variable.woff"
+      fontWeight={900}
+      anchorX={"center"}
+      anchorY={"middle"}
+      characters="ABCDEFGHIJKLMNOPQRSTUVXYZ!,.?'"
+    >
+      {word}
+    </Text>
+  ));
 }
